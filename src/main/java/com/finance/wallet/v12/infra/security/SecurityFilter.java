4900ec8cfe18acdbd7f81b21b22d006f23473cd0
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-12-09 12:03:25 -0300

    feat(Sec): criar classes [ C O R E ] de segurança da aplicação

---
+++ src/main/java/com/finance/wallet/v12/infra/security/SecurityFilter.java
@@ -0,0 +1,51 @@
+package com.finance.wallet.v12.infra.security;
+
+import com.finance.wallet.v12.infra.exceptions.V12UserException;
+import com.finance.wallet.v12.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private static TokenService tokenService;
+    private static UserRepository userRepository;
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository)
+    {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+        if(token != null)
+        {
+            String login = this.tokenService.validateToken(token);
+            if(!login.isEmpty())
+            {
+                UserDetails user = userRepository.findByEmail(login).orElseThrow(() ->
+                    V12UserException.notFound("Usuário não encontrado: " + login));
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
