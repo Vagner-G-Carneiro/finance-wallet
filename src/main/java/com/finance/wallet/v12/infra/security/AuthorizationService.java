b6384b05dad4c97ee45ba0b8365bacfdda6a9bc7
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-12-19 23:41:26 -0300

    refactor(Sec): Trocar de meio de pesquisa ao buscar usuário no banco, sistema busca apenas usuário ativos para validar requisições.

--- src/main/java/com/finance/wallet/v12/infra/security/AuthorizationService.java
+++ src/main/java/com/finance/wallet/v12/infra/security/AuthorizationService.java
@@ -16,7 +16,7 @@ public class AuthorizationService implements UserDetailsService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
+        return this.userRepository.findByEmailAndActiveTrue(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
     }
 }
