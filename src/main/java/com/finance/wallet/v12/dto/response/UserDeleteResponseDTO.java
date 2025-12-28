0c9e3efc959f7da28b3dc63745bfb257a2d3a053
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-12-27 21:07:23 -0300

    refactor(): atualizar nome das classes reccords

--- src/main/java/com/finance/wallet/v12/dto/response/UserDeleteResponseDTO.java
+++ src/main/java/com/finance/wallet/v12/dto/response/UserDeleteResponseDTO.java
@@ -1,6 +1,6 @@
 package com.finance.wallet.v12.dto.response;
 
-public record UserConfirmResponseDTO(
+public record UserDeleteResponseDTO(
         String message,
         boolean successfully){
 }
